b021cc481ca36450be84beae129e8640078866e2
xhhtz <3166469502@qq.com>
2020-09-18 00:05:06 +0800

    新建项目

---
+++ src/main/java/cn/xhhtz/sbe/SbeApplication.java
@@ -0,0 +1,13 @@
+package cn.xhhtz.sbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/xhhtz/sbe/SbeApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.xhhtz.sbe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
